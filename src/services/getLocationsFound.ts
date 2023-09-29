6927ea39b240e9f0256421ae2f1dc25860628f4c
JhonanCaleb <jhonancalebm@gmail.com>
2023-09-29 14:41:54 -0500

    fix http request to https

--- src/services/getLocationsFound.ts
+++ src/services/getLocationsFound.ts
@@ -1,8 +1,8 @@
 import { MY_API_KEY } from '../utils/contants'
 
-const API_WEATHER_ENDPOINT = 'http://api.openweathermap.org/geo/1.0/direct'
+const API_WEATHER_ENDPOINT = 'https://api.openweathermap.org/geo/1.0/direct'
 
-const getLocationsFound = async ({locationKeyword}:{locationKeyword: string}) => {
+const getLocationsFound = async ({ locationKeyword }: { locationKeyword: string }) => {
   try {
     const urlRequest = `${API_WEATHER_ENDPOINT}?q=${locationKeyword}&limit=7&appid=${MY_API_KEY}`
     const req = await fetch(urlRequest)

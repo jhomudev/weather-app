fa4dd9a627aad67b7dd900ede27bdae8afb0f0a4
JhonanCaleb <jhonancalebm@gmail.com>
2023-09-29 14:44:49 -0500

    Broweser to HasRouter

--- src/features/LeftMainBar.tsx
+++ src/features/LeftMainBar.tsx
@@ -1,16 +1,16 @@
-import { BrowserRouter, Outlet, Route, Routes } from 'react-router-dom'
+import { HashRouter, Outlet, Route, Routes } from 'react-router-dom'
 import CoverApp from './CoverApp'
 import FinderLocation from './FinderLocation'
 
 const LeftMainBar = () => {
   return (
     <section className='w-full h-screen md:w-[clamp(260px,30%,479px)]  bg-my_blue-smooth flex gap-5 flex-col items-center justify-between'>
-      <BrowserRouter basename='/weather-app'>
+      <HashRouter basename='/weather-app'>
         <Routes>
           <Route path='/' element={<CoverApp />} />
           <Route path='/search' element={<FinderLocation />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
       <Outlet />
     </section>
   )
